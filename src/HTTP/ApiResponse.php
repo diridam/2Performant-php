a1ff6313198fcf3c7a8a310cb20cdb5218a8a403
mihaitudor <mihai.tudor@thewizards.ro>
2020-04-06 20:30:39 +0300

    updated to get programs Metadata

--- src/HTTP/ApiResponse.php
+++ src/HTTP/ApiResponse.php
@@ -33,8 +33,8 @@ class ApiResponse implements AuthInterface {
         $data = json_decode($response->getBody());
 
         if(isset($data->metadata)) {
+            $data->$expected['metadata'] = $data->metadata;
             $this->meta = $data->metadata;
-            unset($data->metadata);
 
             if(isset($this->meta->deprecations) && (is_array($this->meta->deprecations) || $this->meta->deprecations instanceof Traversable)) {
                 foreach($this->meta->deprecations as $deprecation) {
@@ -217,8 +217,16 @@ class ApiResponse implements AuthInterface {
             if(class_exists($className)) {
                 return new $className($data, $this->owner);
             } elseif(substr($className, -1) == 's' && class_exists(substr($className, 0, -1))) {
-                $className = substr($className, 0, -1);
-                return array_map(function($element) use ($className) { return new $className($element, $this->owner); }, $data);
+
+
+                return $this->mymapper(function($key,$element) use ($className) {
+                    if($key == 'metadata'){
+                        $className = $this->getClassName('metadata');
+                    }else{
+                        $className = substr($className, 0, -1);
+                    }
+                    return new $className($element, $this->owner);
+                }, $data);
             }
         }
 
